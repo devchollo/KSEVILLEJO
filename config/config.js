47c52494dc9750748022ce5cf1d9ba131e9fa8f0
Kent Sevillejo <devchollo@gmail.com>
2025-06-25 11:24:34 -0700

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBQsTlEwelBVfrMwFgCZlTu6MpOheHbPFQ",
+  authDomain: "ksevillejo-a121a.firebaseapp.com",
+  projectId: "ksevillejo-a121a",
+  storageBucket: "ksevillejo-a121a.firebasestorage.app",
+  messagingSenderId: "925221811763",
+  appId: "1:925221811763:web:1633bdcff12accaa8b1546",
+  measurementId: "G-L512CXHRYF",
+};
+
+export default firebaseConfig;
